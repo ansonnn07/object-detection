5e8a22ecd3bf3659f864ea1a955e6b5eaba1ca3e
Anson <roxastan@hotmail.com>
2021-08-06 23:22:48 +0800

    done splitting dataset into folders

--- README.md
+++ README.md
@@ -14,9 +14,11 @@ The Malaysia coins that can be detected are only from the three images below. Th
 **Third Series** <br>
 [![third-series-coins](images/syiling_3.gif)](https://www.bnm.gov.my/-/third-series-of-malaysian-coins)
 
-## Details
+## Details for the steps
 The project starts from downloading images from Google Search using a script prepared based on [PyImageSearch tutorial](https://www.pyimagesearch.com/2017/12/04/how-to-create-a-deep-learning-dataset-using-google-images/). The URLs are scraped using a Javascript script `scrape_image.js` copied from the tutorial. This script will download all the images in the Google image search webpage, therefore, search for the images you want in Google Search, scroll down until the number of images you want (or until the end), then paste all the code here into the Console in your browser (Chrome is recommended). A new file `urls.txt` containing all the image URLs will be downloaded to your computer.
 
 Then, download the images using `download_images.py`. You can choose either to download asynchronously (recommended, much faster) or sequentially. Remember to change the configurations in `config.py` as necessary to make sure the script works as you wanted.
 
-Then, the images will be labelled using [Label Studio](https://labelstud.io/) (**Currently labelling**).
+Then, the images will be labelled using [Label Studio](https://labelstud.io/).
+
+The object detection model (YOLOR in this case) is trained and tested in this [Colab Notebook](https://colab.research.google.com/drive/10pKU_u90_jgfDrG3YsMK7h_RbLW6yZ_P).
---
+++ split_dataset.py
@@ -0,0 +1,67 @@
+import os
+from imutils import paths
+import shutil
+from sklearn.model_selection import train_test_split
+
+BASE_DIR = "coin-images-labels"
+IMAGE_DIR = os.path.join(BASE_DIR, "images")
+LABEL_DIR = os.path.join(BASE_DIR, "labels")
+
+VAL_SIZE = 0.15
+TEST_SIZE = 0.05
+TEST_RUN = 0
+
+# get the image paths and sort them to make sure they align with the label files,
+# the label files have the exact same names as their image files
+# except for different extensions
+image_paths = sorted(list(paths.list_images(IMAGE_DIR)))
+# get the label paths
+label_paths = sorted(os.listdir(LABEL_DIR))
+label_paths = [os.path.join(LABEL_DIR, i) for i in label_paths]
+print(f"Total images = {len(image_paths)}")
+
+
+# split the dataset into ratio of train:val:test of 75%:15%:10%
+# 207:42:28
+X_train, X_val_test, y_train, y_val_test = train_test_split(
+    image_paths, label_paths, test_size=(VAL_SIZE + TEST_SIZE), random_state=42
+)
+X_val, X_test, y_val, y_test = train_test_split(
+    X_val_test,
+    y_val_test,
+    test_size=(TEST_SIZE / (VAL_SIZE + TEST_SIZE)),
+    random_state=42,
+)
+print(f"Total training images = {len(y_train)}")
+print(f"Total validation images = {len(y_val)}")
+print(f"Total testing images = {len(y_test)}")
+
+
+def copy_data(image_paths, label_paths, data_type, test_run=0):
+    assert data_type in ("train", "val", "test")
+    image_dest = os.path.join(BASE_DIR, data_type, "images")
+    label_dest = os.path.join(BASE_DIR, data_type, "labels")
+
+    print(f"[INFO] Copying files from {BASE_DIR} to {image_dest}\n and {label_dest}")
+    for destination in (image_dest, label_dest):
+        # create the directories if not exists
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+
+    for image_path, label_path in zip(image_paths, label_paths):
+        shutil.copy2(image_path, image_dest)
+        shutil.copy2(label_path, label_dest)
+        if test_run:
+            print(f"copying {image_path} to {image_dest}")
+            print(f"copying {label_path} to {label_dest}")
+            break
+
+
+# copy all the images and label files to the respective train, val and test directories
+if TEST_RUN:
+    print("[INFO] Copying only for 1 image in each split ...")
+
+copy_data(X_train, y_train, "train", TEST_RUN)
+copy_data(X_val, y_val, "val", TEST_RUN)
+copy_data(X_test, y_test, "test", TEST_RUN)
+print("[INFO] Files copied successfully.")
